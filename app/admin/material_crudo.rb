19bacf84d09cd4cf78ee2a9e5b058330c42bb89e
mdesanti <mdesanti90@gmail.com>
2014-10-13 14:54:04 +0000

    Dynamic select in active admin

--- app/admin/material_crudo.rb
+++ app/admin/material_crudo.rb
@@ -4,16 +4,29 @@ ActiveAdmin.register MaterialCrudo do
                 :ciudad_id, :barrio_id, :fecha_desde,
                 :fecha_hasta, :discurso, :equipo_id,
                 :disco_id, :ruta_id, :tipo_id, perrele_ids: []
+  
+  controller do
+    def change_prod
+      if params[:client_id].empty?
+        @prods = []
+      else
+        @prods = Client.find_by_id(params[:client_id]).prods
+      end
+      render text: view_context.options_from_collection_for_select(@prods, :id, :descri)
+    end
+  end
 
   form do |f|
     f.inputs "Detalles" do
-      f.input :client_id, label: 'Cliente', as: :select, collection: Client.all.map{|u| ["#{u.name}", u.id]}
-      f.input :prod_id, label: 'Producto', as: :select, collection: Prod.all.map{|u| ["#{u.descri}", u.id]}
+      f.input :client_id, label: 'Cliente', as: :select, collection: Client.all.map{|u| ["#{u.name}", u.id]}, input_html: {
+        onchange: remote_request(:post, :change_prod, {client_id: "$('#material_crudo_client_id').val()"}, :material_crudo_prod_id)
+      }
+      f.input :prod_id, label: 'Producto', as: :select, collection: Client.find_by_id(material_crudo.client_id).prods.map{|u| ["#{u.descri}", u.id]}
       f.input :titulo, as: :string
       f.input :tipo_id, label: 'Tipo', as: :select, collection: Tipo.all.map{|u| ["#{u.descri}", u.id]}
       f.input :descri
-      f.input :ciudad_id, label: 'Ciudad', as: :select, collection: Ciudad.all.map{|u| ["#{u.descri}", u.id]}
-      f.input :barrio_id, label: 'Barrio', as: :select, collection: Barrio.all.map{|u| ["#{u.descri}", u.id]}
+      f.input :ciudad_id, label: 'Ciudad', as: :select, collection: Ciudad.all.map{|u| [u.descri, u.id]}
+      f.input :barrio_id, label: 'Barrio', as: :select, collection: Barrio.all.map{|u| [u.descri, u.id]}
       f.input :fecha_desde
       f.input :fecha_hasta
       f.input :discurso
