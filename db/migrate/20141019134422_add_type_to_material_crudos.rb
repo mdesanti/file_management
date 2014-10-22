50929bbbe14b5e2fe644b61e3ef413f9ac85f864
mdesanti <mdesanti90@gmail.com>
2014-10-22 18:56:15 +0000

    ...

--- db/migrate/20141019134422_add_type_to_material_crudos.rb
+++ db/migrate/20141019134422_add_type_to_material_crudos.rb
@@ -2,6 +2,5 @@ class AddTypeToMaterialCrudos < ActiveRecord::Migration
   def change
     rename_table :material_crudos, :materials
     add_column :materials, :type, :string
-    Material.update_all(type: Material.types[:raw])
   end
 end
